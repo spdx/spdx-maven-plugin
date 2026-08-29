00f976c58bc0da98a5a9ed8ef003e9a5e7748c31
Gary O'Neall <gary@sourceauditor.com>
2026-08-29 11:39:58 -0700

    Use more precise replace regex for https
    Change suggested by Copilot code review
    
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/main/java/org/spdx/maven/utils/SpdxV3LicenseManager.java
+++ src/main/java/org/spdx/maven/utils/SpdxV3LicenseManager.java
@@ -189,7 +189,7 @@ public class SpdxV3LicenseManager
             throw new LicenseManagerException(
                     "Can not map maven license " + mavenLicense.getName() + "  No URL exists to provide a mapping" );
         }
-        String licenseId = this.urlStringToSpdxLicenseId.get( mavenLicense.getUrl().replaceAll("https:", "http:") );
+        String licenseId = this.urlStringToSpdxLicenseId.get( mavenLicense.getUrl().replaceFirst("(?i)^https:", "http:") );
         if ( licenseId == null )
         {
             throw new LicenseManagerException(
